5199cc22cf1814dcc0206290b0a67fa77af41b9c
CrazyAlbert <crazy.albert1301@gmail.com>
2015-05-30 14:30:44 -0600

    Se integraron las imagenes de el formulario

---
+++ test/FacturaTest.java
@@ -0,0 +1,11 @@
+/**
+ * @author Julio Alberto Alvarez Navarrete 256962
+ * @author Jesus Jose
+ * @author Luis Alberto Hernandez Dominguez
+ * @date 25/05/2015
+ *
+ */
+public class FacturaTest {
+
+
+}
